432c4ee40253c456b4df01d1e3883074e9db60ca
ermeng-sspc <2444622785@qq.com>
2024-09-14 11:16:03 +0800

    ``` 重构树/树节点基类 重构工业模型树/树节点实现 重做树打印格式 ```

--- src/imkernel/core/node_base.py
+++ src/imkernel/core/node_base.py
@@ -1,17 +1,39 @@
+from typing import Optional
+
+
 class NodeBase:
-    def __init__(self, id: str, data=None):
-        self.id: str = id
+    """
+    树节点基类
+    """
+
+    def __init__(self, identification: str, desc: str = None, data=None):
+        """
+        初始化
+        :param identification:标识符
+        :param desc:描述
+        :param data:扩展数据字段
+        """
+        self.id = identification
+        self.desc = desc
         self.data = data
-        self.parent: NodeBase = None
-        self.children: list[NodeBase] = []
+        self.parent: Optional["NodeBase"] = None
+        self.children: list["NodeBase"] = []
 
     def add_child(self, child_node: "NodeBase") -> None:
+        """
+        添加子节点
+        :param child_node:
+        """
         child_node.parent = self
         self.children.append(child_node)
 
     def remove_child(self, child_node: "NodeBase") -> None:
+        """
+        移除子节点
+        :param child_node:
+        """
         self.children.remove(child_node)
         child_node.parent = None
 
     def __str__(self) -> str:
-        return f"NodeBase(id={self.id}, data={self.data})"
+        return f"NodeBase(id={self.id}, desc={self.desc},data={self.data})"
