0b7c6ccedd03bdc19eb195de2b150a8b388c817b
JMR <853320864@qq.com>
2024-10-09 13:31:18 +0800

    新增occ读取展示功能，添加函数使得能够使用展示step模型

--- src/imkernel/v3d/__init__.py
+++ src/imkernel/v3d/__init__.py
@@ -1,2 +1,2 @@
-from .occ_v import show_occ_shape
+from .occ_v import show_occ_shape, show_stp_shape
 from .vtk_utils import show_obj, show_points_from_list, show_multi_points_from_list
--- src/imkernel/v3d/occ_v.py
+++ src/imkernel/v3d/occ_v.py
@@ -19,7 +19,8 @@ from OCC.Core.TDocStd import TDocStd_Document
 from OCC.Core.TopoDS import TopoDS_Shape
 from OCC.Core.UnitsMethods import unitsmethods
 from OCC.Core.XCAFDoc import XCAFDoc_DocumentTool
-
+from OCC.Core.STEPControl import STEPControl_Reader
+from OCC.Core.IFSelect import IFSelect_RetDone
 from imkernel.v3d.occ_utils import shape_to_obj
 from imkernel.v3d.vtk_utils import show_obj
 
@@ -30,3 +31,23 @@ def show_occ_shape(shape: TopoDS_Shape):
     if not obj_file_path:
         raise Exception("obj生成失败")
     show_obj(obj_file_path)
+
+
+def show_stp_shape(stp_file_path: str):
+    # 生成一个 step 模型类
+    reader = STEPControl_Reader()
+    # 加载一个文件并且返回一个状态枚举值
+    status = reader.ReadFile(stp_file_path)
+    # 如果正常执行且有模型
+    try:
+        if status == IFSelect_RetDone:  # check status
+            # 执行步骤文件转换
+            ok = reader.TransferRoot(1)
+            # 返回转换后的形状
+            shape = reader.Shape(1)
+        else:
+            print(f"文件{stp_file_path}加载失败，返回测试 Shape")
+            shape = BRepPrimAPI_MakeBox(1, 1, 1).Shape()
+        show_occ_shape(shape)
+    except Exception as e:
+        print(f"{e} 文件{stp_file_path}加载失败")
